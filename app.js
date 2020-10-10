7f5d96e2212751c548b36afd5a5237c8579208be
qtnkaca1003 <54465270+qtnkaca1003@users.noreply.github.com>
2020-10-10 20:25:15 +0700

    font-end-from

--- app.js
+++ app.js
@@ -15,11 +15,12 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
   }
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 //Routes
 app.get('/', (req, res, next) =>{
-    return res.status(200).json({
-        Message: "Oke!"
-    })
+    return res.status(200).render('./khachhang/index',{});
 })
 //Catch 404 Erros and forward them to error handler
 app.use((req, res, next) => {
