eab5be06bf4305b322838a4d8ab78453172c918e
doanvanduytam1999 <44457969+doanvanduytam1999@users.noreply.github.com>
2020-10-10 17:09:20 +0700

    update connect DB

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-/* const mongoose = require('mongoose'); */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-//const DB = "mongodb://appDev:Dev2020!@113.161.86.105:2727/isms";
-/* const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
+const DB = process.env.DATABASE.replace(
+  '<password>',
   process.env.DATABASE_PASSWORD
 );
 mongoose
@@ -15,9 +14,15 @@ mongoose
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false
-  })
-  .then(() => console.log('DB connection successful!')); */
-
+  }, function(err){
+    if(err){
+      console.log("Mongo connect error: "+ err);
+    }
+    else{
+      console.log("DB connection successful!")
+    }
+  });
+  
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
